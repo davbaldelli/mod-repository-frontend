b24f4622bb10e40c0680d027d7013b5d89da324c
Davide Baldelli <davbaldelli@gmail.com>
2021-12-09 00:18:39 +0100

    minor changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -126,7 +126,11 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
+      lang: 'en',
+      name: 'AC Mod Repository',
+      short_name: 'AC Mod Repo',
+      description: "The best collection of quality cars and tracks mods for Assetto Cosa. Every mod is free and tested and up-to-date",
+      useWebmanifestExtension: false
     },
     meta : {
       title: "AC Mod Repository",
