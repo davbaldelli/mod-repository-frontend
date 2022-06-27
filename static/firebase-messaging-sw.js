91773b0e22b70e3648daff26d061653e881e53ed
Davide Baldelli <davbaldelli@gmail.com>
2022-06-27 15:57:34 +0200

    updated nuxt version

--- static/firebase-messaging-sw.js
+++ static/firebase-messaging-sw.js
@@ -1,9 +1,9 @@
 
 importScripts(
-  'https://www.gstatic.com/firebasejs/9.6.0/firebase-app-compat.js'
+  'https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js'
 )
 importScripts(
-  'https://www.gstatic.com/firebasejs/9.6.0/firebase-messaging-compat.js'
+  'https://www.gstatic.com/firebasejs/9.8.4/firebase-messaging.js'
 )
 firebase.initializeApp({"apiKey":"AIzaSyDIYM65D5DNLNsOSUToov_JcJEh6AupUlg","authDomain":"ac-mod-repo.firebaseapp.com","projectId":"ac-mod-repo","storageBucket":"ac-mod-repo.appspot.com","messagingSenderId":"315180351715","appId":"1:315180351715:web:d5bef233ddee49100b05d6","measurementId":"G-KFQQFD5MJW"})
 
@@ -14,7 +14,7 @@ const messaging = firebase.messaging()
 // Setup event listeners for actions provided in the config:
 self.addEventListener('notificationclick', function(e) {
   const actions = [{"action":"car_updated","url":"\u002Flogs"},{"action":"car_added","url":"\u002Flogs"},{"action":"track_updated","url":"\u002Flogs"},{"action":"track_added","url":"\u002Flogs"}]
-  const action = actions.find(x => x.action === e.action)
+  const action = actions.find(x => x.id === e.action.action)
   const notification = e.notification
 
   if (!action) return
@@ -22,20 +22,5 @@ self.addEventListener('notificationclick', function(e) {
   if (action.url) {
     clients.openWindow(action.url)
     notification.close()
-  }
-})
-
-self.addEventListener('push', function (e) {
-  let data = e.data.json()
-  console.log(data)
-  const options = {
-    title: data.notification.title,
-    body: data.notification.body,
-    icon: data.notification.icon,
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: '2'
-    },
   }
 })
