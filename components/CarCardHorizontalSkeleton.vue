38a7a7dd855bdd5c68c7ebd7ecb574669131b9ad
Davide Baldelli <davbaldelli@gmail.com>
2022-11-30 19:54:56 +0100

    removed unecessary code

---
+++ components/CarCardHorizontalSkeleton.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "CarCardHorizontalSkeleton"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/TrackCardSkeleton.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+name: "TrackCardSkeleton"
+}
+</script>
+
+<style scoped>
+
+</style>
