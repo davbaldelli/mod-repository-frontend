2e6d654521c5f09eae94b01aa101e13502af1316
Davide Baldelli <davbaldelli@gmail.com>
2022-12-08 09:32:12 +0100

    added darker background

--- components/BrandCard.vue
+++ components/BrandCard.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-card class="pa-5" :to="`/cars/${brand.name}/`">
+  <v-card class="pa-5 light-grey" :to="`/cars/${brand.name}/`">
     <v-img contain :src="brand.logo" height="200px" ></v-img>
-    <v-card-title v-text="brand.name" class="justify-center"></v-card-title>
+    <v-card-title v-text="brand.name" class="justify-center" ></v-card-title>
   </v-card>
 </template>
 
@@ -13,5 +13,7 @@ export default {
 </script>
 
 <style scoped>
-
+  .light-grey{
+    background-color : #F5F5F5;
+  }
 </style>
--- components/NationCard.vue
+++ components/NationCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card class="pa-5" :to="`/tracks/${nation.name}/`">
+  <v-card class="pa-2 light-grey" :to="`/tracks/${nation.name}/`">
     <v-img contain :src="`https://flagcdn.com/h240/${nation.code}.png`" height="240"></v-img>
     <v-card-title v-text="nation.name" class="justify-center"></v-card-title>
   </v-card>
@@ -13,5 +13,7 @@ export default {
 </script>
 
 <style scoped>
-
+  .light-grey{
+    background-color : #F5F5F5;
+  }
 </style>
