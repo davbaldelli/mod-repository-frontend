1f88af0a0fc47d52a55490718611ee3928782c58
Davide Baldelli <davbaldelli@gmail.com>
2022-12-30 00:20:07 +0100

    created track and car detail panel

---
+++ components/CarDetailPanel.vue
@@ -0,0 +1,129 @@
+<template>
+  <v-row>
+    <v-col>
+      <v-row>
+        <v-col cols="12">
+          <v-img :src="car.images[0].url" contain></v-img>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <h1 class="text-h3">{{ `${car.brand.name} ${car.modelName}` }}</h1>
+        </v-col>
+      </v-row>
+      <v-row class="mt-0">
+        <v-col>
+          <v-rating v-model="car.rating" background-color="orange lighten-3" color="orange" dense
+                    length="10"
+                    readonly
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn v-if="(!car.premium || isPremium) && !car.official" block :href="car.downloadLink" color="primary"
+                 rel="noopener" target="_blank">Download
+          </v-btn>
+          <v-btn v-else-if="!car.official" block :href="car.source" color="orange" rel="noopener" target="_blank">Buy
+            it here!
+          </v-btn>
+          <v-btn v-else disabled block>Official Content</v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-card class="pa-4">
+            <v-row>
+              <v-col cols="12" md="6">
+                <v-list>
+                  <v-list-item>
+                    <strong>Brand</strong>
+                    <v-spacer/>
+                    {{ car.brand.name }}
+                  </v-list-item>
+                  <v-divider></v-divider>
+                  <v-list-item>
+                    <strong>Nation</strong>
+                    <v-spacer/>
+                    {{ car.brand.nation.name }}
+                  </v-list-item>
+                  <v-divider></v-divider>
+                  <v-list-item>
+                    <strong>Year</strong>
+                    <v-spacer/>
+                    {{ car.year }}
+                  </v-list-item>
+                  <v-divider/>
+                  <v-list-item>
+                    <strong>Author</strong>
+                    <v-spacer/>
+                    <a v-if="car.author.link && car.author.link !== '#'" :href="car.author.link" rel="noopener"
+                       target="_blank">{{ car.author.name }}</a>
+                    <span v-else>{{ car.author.name }}</span>
+                  </v-list-item>
+                  <v-divider/>
+                  <v-list-item>
+                    <strong>Mod Version</strong>
+                    <v-spacer/>
+                    {{ car.version }}
+                  </v-list-item>
+                  <v-divider/>
+                </v-list>
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-list>
+                  <v-list-item>
+                    <strong>Power</strong>
+                    <v-spacer/>
+                    {{ car.bhp }} bhp
+                  </v-list-item>
+                  <v-divider/>
+                  <v-list-item>
+                    <strong>Torque</strong>
+                    <v-spacer/>
+                    {{ car.torque }} Nm
+                  </v-list-item>
+                  <v-divider/>
+                  <v-list-item>
+                    <strong>Top Speed</strong>
+                    <v-spacer/>
+                    {{ car.topSpeed }} Km/h
+                  </v-list-item>
+                  <v-divider/>
+                  <v-list-item>
+                    <strong>Transmission</strong>
+                    <v-spacer/>
+                    {{ car.transmission }}
+                  </v-list-item>
+                  <v-divider/>
+                  <v-list-item>
+                    <strong>Drivetrain</strong>
+                    <v-spacer/>
+                    {{ car.drivetrain }}
+                  </v-list-item>
+                  <v-divider/>
+                </v-list>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "CarDetailPanel",
+  props: ['car'],
+  computed: {
+    isPremium() {
+      return this.$store.getters['authentication/isPremium']
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/tracks/detail/_id.vue
+++ pages/tracks/detail/_id.vue
@@ -30,84 +30,9 @@
             </v-row>
           </v-col>
         </v-row>
-        <v-row v-if="track">
-          <v-col>
-            <v-row>
-              <v-col cols="12">
-                <v-img :src="track.images[0].url" contain></v-img>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
-                <h1 class="text-h3">{{ track.name }}</h1>
-              </v-col>
-            </v-row>
-            <v-row class="mt-0">
-              <v-col>
-                <v-rating v-model="track.rating" background-color="orange lighten-3" color="orange" dense length="10"
-                          readonly
-                />
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
-                <v-card class="pa-4">
-                  <v-row>
-                    <v-col cols="12" md="6">
-                      <v-list>
-                        <v-list-item>
-                          <strong>Location</strong><v-spacer/>{{ `${track.location}, ${track.nation.name}` }}
-                        </v-list-item>
-                        <v-divider/>
-                        <v-list-item>
-                          <strong>Year</strong><v-spacer/>{{ track.year }}
-                        </v-list-item>
-                        <v-divider/>
-                      </v-list>
-                    </v-col>
-                    <v-col cols="12" md="6">
-                      <v-list>
-                        <v-list-item>
-                          <strong>Author</strong><v-spacer/>
-                          <a  v-if="track.author.link && track.author.link !== '#'" :href="track.author.link" rel="noopener" target="_blank">{{ track.author.name }}</a>
-                          <span v-else>{{ track.author.name }}</span>
-                        </v-list-item>
-                        <v-divider/>
-                        <v-list-item>
-                          <strong>Mod Version</strong><v-spacer/>{{ track.version }}
-                        </v-list-item>
-                        <v-divider/>
-                      </v-list>
-                    </v-col>
-                  </v-row>
-                </v-card>
-              </v-col>
-            </v-row>
-            <v-row class="mt-6">
-              <v-col>
-                <h2 class="text-h4">Layouts</h2>
-              </v-col>
-            </v-row>
-            <v-row class="mt-1">
-              <v-col>
-                <v-card v-for="layout in track.layouts" :key="layout.name" class="pa-4 mb-2">
-                  <v-row>
-                    <v-col cols="12" md="4"><strong>Name</strong> {{ layout.name }}</v-col>
-                    <v-col cols="12" md="4"><strong>Length</strong> {{ layout.lengthM }}m</v-col>
-                    <v-col cols="12" md="4"><strong>Type</strong> {{ layout.category }}</v-col>
-                  </v-row>
-                </v-card>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
-                <v-btn v-if="(!track.premium || isPremium) && !track.official"  block :href="track.downloadLink" color="primary" rel="noopener" target="_blank">Download</v-btn>
-                <v-btn v-else-if="!track.official" block :href="track.source" color="orange" rel="noopener" target="_blank">Buy it here!</v-btn>
-                <v-btn v-else disabled block>Official Content</v-btn>
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-row>
+        <client-only v-else>
+          <track-detail-panel :track="track"/>
+        </client-only>
       </v-col>
       <v-col cols="0" lg="4" md="3"/>
     </v-row>
@@ -160,9 +85,6 @@ export default {
           to: `/tracks/${this.track.id}/`,
         },
       ]
-    },
-    isPremium(){
-      return this.$store.getters['authentication/isPremium']
     },
     loading () {
       return this.$store.getters['track/loadingTracks']
