c701f81990719c4c1891489e5de980bba6607810
Davide Baldelli <davbaldelli@gmail.com>
2023-01-04 00:37:12 +0100

    removed dark theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -90,7 +90,6 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/fontawesome.js',
-    '~/plugins/theme.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
