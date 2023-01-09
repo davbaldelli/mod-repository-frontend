aaec193d3ec9b16d31c46ce633c06c936684b444
Davide Baldelli <davbaldelli@gmail.com>
2023-01-09 09:28:55 +0100

    rounded flags

--- components/nation/Card.vue
+++ components/nation/Card.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card :class="`pa-5 ${light ? 'light-grey' : ''}`" :to="`/tracks/${nation.name}`" nuxt>
-    <v-img contain :src="`https://flagcdn.com/h240/${nation.code}.png`" height="240" :alt="`${nation.name} flag`"/>
+    <v-img contain :src="`https://flagcdn.com/h240/${nation.code}.png`" height="240" :alt="`${nation.name} flag`" class="rounded"/>
     <v-card-title v-text="nation.name" class="justify-center"></v-card-title>
   </v-card>
 </template>
