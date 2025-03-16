5c70b822f527b2bacb6baa8893ba80395f64e40a
dbell <davide.baldelli@nonstudio.org>
2025-03-16 01:10:05 +0100

    changed remote

--- _services/config.js
+++ _services/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://api.acmodrepository.com:6316'
+export const API_URL = 'https://api.acmodrepository.com'
 // export const API_URL = 'https://home.davidebaldelli.it:6316'
